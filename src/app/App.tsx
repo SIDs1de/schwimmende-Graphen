2a56756b5c1807ba0303530287d74ac3d44ca39c
Игорь Денисенков <igorekdenisenkov@yandex.ru>
2026-07-27 22:17:52 +0300

    KAN-5: feat: типизация для файлов стилей

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,10 @@ import type { FC } from "react";
 import { RouterProvider } from "react-router";
 import { QueryClientProvider } from "./data-fetching";
 import { router } from "./router";
-import styles from "./App.module.scss";
+import "normalize.css";
 
 export const App: FC = () => (
-  <div className={styles.app}>
-    <QueryClientProvider>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </div>
+  <QueryClientProvider>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
